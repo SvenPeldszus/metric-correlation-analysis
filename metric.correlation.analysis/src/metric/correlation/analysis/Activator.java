46290185215f478544287321b1f92ad9bcff9898
Sven Peldszus <speldszus@uni-koblenz.de>
2020-12-18 16:03:32 +0100

    Added missing new activator

---
+++ metric.correlation.analysis/src/metric/correlation/analysis/Activator.java
@@ -0,0 +1,7 @@
+package metric.correlation.analysis;
+
+import org.eclipse.core.runtime.Plugin;
+
+public class Activator extends Plugin {
+	public static final String PLUGIN_ID = "metric.correlation.analysis";
+}
